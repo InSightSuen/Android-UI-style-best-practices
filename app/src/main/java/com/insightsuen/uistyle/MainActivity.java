83a61ae40a6952271b995b3dc62009dabe682fe8
InSight Suen <hellosession@gmail.com>
2017-11-17 14:49:46 +0800

    add main activity

---
+++ app/src/main/java/com/insightsuen/uistyle/MainActivity.java
@@ -0,0 +1,11 @@
+package com.insightsuen.uistyle;
+
+import com.insightsuen.base.view.BaseActivity;
+
+/**
+ * Created by InSight Suen on 2017/11/17.
+ * Main Activity
+ */
+
+public class MainActivity extends BaseActivity {
+}
