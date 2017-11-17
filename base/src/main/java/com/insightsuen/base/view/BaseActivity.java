aadc733607c3d827b748b3e3207b5c1497efe84b
InSight Suen <hellosession@gmail.com>
2017-11-17 14:49:34 +0800

    add base activity

---
+++ base/src/main/java/com/insightsuen/base/view/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.insightsuen.base.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by InSight Suen on 2017/11/17.
+ * Base of all Activities
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
