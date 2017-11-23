df3e57390fc5ee966cdb5a42b18f7bac0bc59441
InSight Suen <hellosession@gmail.com>
2017-11-23 14:51:54 +0800

    add switch theme

--- app/src/main/java/com/insightsuen/uistyle/MainActivity.java
+++ app/src/main/java/com/insightsuen/uistyle/MainActivity.java
@@ -5,19 +5,20 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 
-import com.insightsuen.base.view.BaseActivity;
+import com.insightsuen.uistyle.base.UiStyleActivity;
 
 /**
  * Created by InSight Suen on 2017/11/17.
  * Main Activity
  */
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends UiStyleActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolBar();
     }
 
     public void onClickTextStyle(View view) {
